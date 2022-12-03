ac170ba79030e2bdacd94694d617c18a92bbe6cb
Karl Fredriksson <karlpafredriksson@gmail.com>
2022-12-03 14:55:18 +0100

    everything except db

--- controllers/stuffController.js
+++ controllers/stuffController.js
@@ -1,1 +1,24 @@
 'use strict';
+const stuff = require('../models/stuffModel');
+
+exports.getStuff = (req, res) => {
+    try {
+        const stf = stuff.getAll();
+        res.status(200);
+        res.send(stf);
+    } catch (e) {
+        console.log('error ', e); 
+        res.sendStatus(500);
+    }
+};
+
+exports.postStuff = (req, res) => {
+    try {
+        stuff.postOne(req.body);
+        res.status(201);
+        res.send();
+    } catch (e) {
+        console.log('error ', e); 
+        res.sendStatus(500);
+    }
+};
